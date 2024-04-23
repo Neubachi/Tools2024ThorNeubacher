b4187446ed08e2d0e5c072a0eb5a90da35ddd495
Neubachi <neubacher.fabian@hotmail.com>
2024-04-23 21:44:49 +0200

    method added

--- zuperZimon_Neubachi_android/PPT_Uebung04/src/App.java
+++ zuperZimon_Neubachi_android/PPT_Uebung04/src/App.java
@@ -1,4 +1,9 @@
 public class App {
+
+    public void thisIsANewMethod() {
+
+    }
+
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
     }
