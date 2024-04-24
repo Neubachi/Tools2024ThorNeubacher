e04c0ff8cfd3c59b5683c76272b3d5030fb24d0b
zuperZimon <simon.thor2003mail.com>
2024-04-24 18:46:57 +0200

    eawas

--- zuperZimon_Neubachi_android/PPT_Uebung04/src/App.java
+++ zuperZimon_Neubachi_android/PPT_Uebung04/src/App.java
@@ -3,15 +3,10 @@ public class App {
     public void thisIsANewMethod() {
 
         String Git = "scheiße";
-    }
-
-X
         for (int i = 0; i < 10; i++) {
 
         }
-
     }
-
 
     // ich finde git scheiße
 
